0df5087da717ab045db15a65a6ed19fbfabf4c24
Chime Ogbuji <chimezie@gmail.com>
2024-03-29 08:20:04 -0400

    Add update of progress bar in training callback

--- src/mlx_tuning_fork/reporting.py
+++ src/mlx_tuning_fork/reporting.py
@@ -5,9 +5,27 @@ except ImportError:
     wandb = None
 
 
-class WandbCallback(TrainingCallback):
+class BaseTrainingCallback(TrainingCallback):
+    def __init__(self, progress_bar):
+        self.iteration = 0
+        self.progress_bar = progress_bar
+
+    def update_progress(self, info):
+        iteration = info["iteration"]
+        self.progress_bar.update(iteration - self.iteration)
+        self.iteration = iteration
 
     def on_train_loss_report(self, train_info):
+        self.update_progress(train_info)
+
+    def on_val_loss_report(self, val_info):
+        self.update_progress(val_info)
+
+
+class WandbCallback(BaseTrainingCallback):
+
+    def on_train_loss_report(self, train_info):
+        super().on_train_loss_report(train_info)
         if wandb is None:
             raise ImportError('wandb module not available.  Install with `pip install wandb`')
         try:
@@ -16,6 +34,7 @@ class WandbCallback(TrainingCallback):
             print(f"logging to wandb failed: {e}")
 
     def on_val_loss_report(self, val_info):
+        super().on_train_loss_report(val_info)
         if wandb is None:
             raise ImportError('wandb module not available.  Install with `pip install wandb`')
         try:
