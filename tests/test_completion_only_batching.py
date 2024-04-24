e381a19b9a878f82d427509d07ab5e6002a3b6f2
Chime Ogbuji <chimezie@gmail.com>
2024-04-24 15:20:18 -0400

    Consolidated common logic

--- tests/test_completion_only_batching.py
+++ tests/test_completion_only_batching.py
@@ -1,46 +1,39 @@
+import pytest
 import os
 import mlx.core as mx
 from mlx_lm.utils import load
+import numpy as np
 
 from mlx_tuning_fork.tuning.utils import create_delineated_batches
 
 
 class TestCreateDelineatedBatches:
-    def test_basic_no_truncation(self):
-        model, tokenizer = load(os.environ.get("MLX_MODEL_PATH"))
-        prompt = 'My name is: '
-        prompt_ids = tokenizer.encode(prompt, add_special_tokens=False)
-        tokenized_prompt_length = len(prompt_ids)
-        output_text = ['Foo', 'Mandalorian', 'San The country with no name']
-        input_text = [prompt] * len(output_text)
-        batch, input_lengths, lengths = create_delineated_batches(input_text, output_text, tokenizer, max_seq_length=20)
-        assert all([len(i) == 16 for i in batch])
-        assert all([i == tokenized_prompt_length for i in input_lengths])
-        for idx, output in enumerate(output_text):
+    prompt = 'My name is: '
+    output_text = ['Foo', 'Mandalorian', 'San the country with no name']
+
+    def setup_method(self):
+        _, self.tokenizer = load(os.environ.get("MLX_MODEL_PATH"))
+        prompt_ids = self.tokenizer.encode(self.prompt)
+        self.tokenized_prompt_length = len(prompt_ids)
+        self.input_text = [self.prompt] * len(self.output_text)
+
+    def _run_test(self, max_seq_length, expected_length):
+        batch, input_lengths, lengths = create_delineated_batches(self.input_text, self.output_text, self.tokenizer,
+                                                                  max_seq_length=max_seq_length)
+        assert all([len(i) == expected_length for i in batch])
+        assert mx.all(input_lengths == self.tokenized_prompt_length).item()
+        for idx, output in enumerate(self.output_text):
             batch_item = batch[idx]
-            output_ids = tokenizer.encode(output, add_special_tokens=False)
+            output_ids = self.tokenizer.encode(output)
             tokenized_output_length = len(output_ids)
-            non_padding_length = tokenized_output_length + tokenized_prompt_length
+            non_padding_length = tokenized_output_length + self.tokenized_prompt_length
             padding = batch_item[non_padding_length:]
-            expected_padding = mx.array([0] * (16 - non_padding_length))
+            expected_padding = mx.array([0] * (expected_length - non_padding_length))
             assert mx.array_equal(padding, expected_padding)
 
+    def test_basic_no_truncation(self):
+        self._run_test(20, 16)
+
     def test_basic_w_truncation(self):
-        model, tokenizer = load(os.environ.get("MLX_MODEL_PATH"))
-        prompt = 'My name is: '
-        prompt_ids = tokenizer.encode(prompt, add_special_tokens=False)
-        tokenized_prompt_length = len(prompt_ids)
-        output_text = ['Foo', 'Mandalorian', 'San The country with no name']
-        input_text = [prompt] * len(output_text)
-        batch, input_lengths, lengths = create_delineated_batches(input_text, output_text, tokenizer, max_seq_length=10)
-        assert all([len(i) == 10 for i in batch])
-        assert all([i == tokenized_prompt_length for i in input_lengths])
-        for idx, output in enumerate(output_text):
-            batch_item = batch[idx]
-            output_ids = tokenizer.encode(output, add_special_tokens=False)
-            tokenized_output_length = len(output_ids)
-            non_padding_length = tokenized_output_length + tokenized_prompt_length
-            padding = batch_item[non_padding_length:]
-            expected_padding = mx.array([0] * (10 - non_padding_length))
-            assert mx.array_equal(padding, expected_padding)
+        self._run_test(10, 10)
 
