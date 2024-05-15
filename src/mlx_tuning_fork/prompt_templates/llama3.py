385dafb06ca398f8356d5dd433ac4bce17a4931d
Chime Ogbuji <chimezie@gmail.com>
2024-05-15 13:36:13 -0400

    Fixes to Llama 3 prompt format

--- src/mlx_tuning_fork/prompt_templates/llama3.py
+++ src/mlx_tuning_fork/prompt_templates/llama3.py
@@ -3,9 +3,10 @@ from typing import Dict
 
 #https://github.com/OoriData/OgbujiPT/pull/70
 LLAMA3_DELIMITERS = {
-    pdelim.PRECONTEXT: '<|start_header_id|>system<|end_header_id|>',
+    pdelim.FIXED_PREAMBLE: '<|begin_of_text|>',
+    pdelim.PRECONTEXT: '<|start_header_id|>system<|end_header_id|>\n\n',
     pdelim.POST_ALL_CONTEXT: '<|eot_id|>',
-    pdelim.PREQUERY: '<|start_header_id|>user<|end_header_id|>',
+    pdelim.PREQUERY: '<|start_header_id|>user<|end_header_id|>\n\n',
     pdelim.POSTQUERY: '<|eot_id|>'
 }
 
@@ -17,7 +18,7 @@ class TrainingRecordHandler:
 
     @classmethod
     def get_output(cls, record) -> str:
-        return f"<|start_header_id|>assistant<|end_header_id|>{record['output']}<|eot_id|>"
+        return f"<|start_header_id|>assistant<|end_header_id|>\n\noutput<|eot_id|>"
 
     @classmethod
     def get_delimiters(cls) -> Dict:
